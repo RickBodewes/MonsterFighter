cab81fa399704e8fb6bd575d95435719964bf0e0
RickBodewes <Rick@planet.nl>
2021-06-30 11:28:13 +0200

    add main menu and clickable play button (also starts the game)

---
+++ Assets/scripts/mainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    //  Fired when the play button is pressed
+    public void Play()
+    {
+        SceneManager.LoadScene("play");
+    }
+}
