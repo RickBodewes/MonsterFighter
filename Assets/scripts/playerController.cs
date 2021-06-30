769ed2b4c8c55b66b19dc2cdfa0cc89d8b156334
RickBodewes <Rick@planet.nl>
2021-06-30 10:33:32 +0200

    Import all assets reusable from previous game

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    //main player values
+    [Header("player stats")]
+    public int speed;
+    public int jumpPower;
+
+    //private variables for the engine
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //assigning the animator component
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixuedUpdate()
+    {
+        
+    }
+}
